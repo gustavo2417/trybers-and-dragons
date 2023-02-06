c1f0943f2bf986bef0ac86db772de531fa0739f4
Gustavo <gustavodarocha241@gmail.com>
2023-02-06 01:18:27 -0300

    requisitos 9 e 10 completos

---
+++ src/Battle/PVP.ts
@@ -0,0 +1,28 @@
+import Character from '../Character';
+import Battle from './Battle';
+import Fighter from '../Fighter';
+
+class PVP extends Battle {
+  private _Character1: Character | Fighter;
+  private _Character2: Character | Fighter;
+
+  constructor(
+    Character1: Character | Fighter,
+    Character2: Character | Fighter,
+  ) {
+    super(Character2);
+
+    this._Character1 = Character1;
+    this._Character2 = Character2;
+  }
+
+  fight(): number {
+    while (this._Character1.lifePoints > 0 && this._Character2.lifePoints > 0) {
+      this._Character1.attack(this._Character2);
+      this._Character2.attack(this._Character1);
+    }
+    return this._Character1.lifePoints === -1 ? -1 : 1;
+  }
+}
+
+export default PVP;
